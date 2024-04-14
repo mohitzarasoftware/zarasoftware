fbdc9a51e0f8da0a74437878bc2ac94469be072c
mohitzarasoftware <163724657+mohitzarasoftware@users.noreply.github.com>
2024-04-14 13:03:06 -0700

    d

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -10,8 +10,12 @@ var swiper = new Swiper(".footerlogo", {
       clickable: true,
     },
     breakpoints: {
+      0: {
+        slidesPerView:2,
+        spaceBetween: 10,
+      },
       640: {
-        slidesPerView: 2,
+        slidesPerView: 3,
         spaceBetween: 10,
       },
       768: {
