d9e3b6ae06d0451bbf4f325cb0f6eca782fd1e17
mohitzarasoftware <163724657+mohitzarasoftware@users.noreply.github.com>
2024-05-06 22:42:48 +0530

    g

--- assets/js/index.js
+++ assets/js/index.js
@@ -101,7 +101,7 @@ let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning =3000;
-let timeAutoNext = 7000;
+let timeAutoNext = 12000;
 // let timeAutoNext = 145000;
 
 nextDom.onclick = function(){
